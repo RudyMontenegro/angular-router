c9c0f978f2b41e12b4d631f4f3c058039a6eb6c6
RudyMontenegro <rudymontenegro4@gmail.com>
2023-04-19 23:28:07 -0400

    rm-evitando doble subscripcion

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,40 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
-import { ProductsService } from '../../services/products.service';
-import {
-  Product,
-} from '../../models/product.model';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { Product } from '../../models/product.model';
+import { ProductsService } from './../../services/products.service';
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  template: `<app-products
+    [products]="products"
+    (loadMore)="onLoadMore()"
+  ></app-products>`,
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent implements OnInit {
-  products: Product[] = [];
   categoryId: string | null = null;
+  limit = 10;
+  offset = 0;
+  products: Product[] = [];
+
   constructor(
     private route: ActivatedRoute,
     private productsService: ProductsService
-  ) {
+  ) {}
 
-  }
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      param => {
-       this.categoryId = param.get('id');// id tiene que ser igual al nombre en app-routing
-       if(this.categoryId){
-        this.productsService.getByCategory(this.categoryId).subscribe
-        ( data =>
-          {
-            this.products = data;
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          this.categoryId = params.get('id');
+          if (this.categoryId) {
+            return this.productsService.getByCategory(
+              this.categoryId,
+              this.limit,
+              this.offset
+            );
           }
-        )
-       }
-       
-      }
-    )
+          return [];
+        })
+      )
+      .subscribe((data) => {
+        this.products = data;
+      });
+  }
+
+  onLoadMore() {
+    if (this.categoryId) {
+      this.productsService
+        .getByCategory(this.categoryId, this.limit, this.offset)
+        .subscribe((data) => {
+          this.products = this.products.concat(data);
+          this.offset += this.limit;
+        });
+    }
   }
 }
-
-
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -12,28 +12,28 @@ import { environment } from './../../environments/environment';
 })
 export class ProductsService {
 
-  private apiUrl = `${environment.API_URL}/api/products`;
+  private apiUrl = `${environment.API_URL}/api`;
 
   constructor(
     private http: HttpClient
   ) { }
 
-  getByCategory(categoryId:string,limit?: number, offset?: number ){
+  getByCategory(categoryId: string, limit?: number, offset?: number){
     let params = new HttpParams();
-    if (limit && offset) {
+    if (limit && offset != null) {
       params = params.set('limit', limit);
-      params = params.set('offset', limit);
+      params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(`${this.apiUrl}/category/${categoryId}`, { params, context: checkTime() })
+    return this.http.get<Product[]>(`${this.apiUrl}/categories/${categoryId}/products`, { params })
   }
 
   getAll(limit?: number, offset?: number) {
     let params = new HttpParams();
-    if (limit && offset) {
+    if (limit && offset != null) {
       params = params.set('limit', limit);
-      params = params.set('offset', limit);
+      params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params, context: checkTime() })
+    return this.http.get<Product[]>(`${this.apiUrl}/products`, { params, context: checkTime() })
     .pipe(
       retry(3),
       map(products => products.map(item => {
@@ -75,10 +75,10 @@ export class ProductsService {
   }
 
   update(id: string, dto: UpdateProductDTO) {
-    return this.http.put<Product>(`${this.apiUrl}/${id}`, dto);
+    return this.http.put<Product>(`${this.apiUrl}/products/${id}`, dto);
   }
 
   delete(id: string) {
-    return this.http.delete<boolean>(`${this.apiUrl}/${id}`);
+    return this.http.delete<boolean>(`${this.apiUrl}/products/${id}`);
   }
 }
