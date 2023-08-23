62586ae4b0f07af965f6107e79697fdba7fea55a
RudyMontenegro <rudymontenegro4@gmail.com>
2023-08-22 21:04:18 -0400

    avances

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { TokenService } from './../services/token.service'
-
+import { AuthService } from './../services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard  {
 
   constructor(
+    private authService: AuthService,
     private tokenService: TokenService,
     private router: Router
   ){
@@ -19,12 +21,22 @@ export class AuthGuard  {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const token = this.tokenService.getToken();
-    if(!token){
-      this.router.navigate(['/home']);
-      return false
-    }
-    return true;
+    // const token = this.tokenService.getToken();
+    // if(!token){
+    //   this.router.navigate(['/home']);
+    //   return false
+    // }
+    // return true;
+      return this.authService.user$
+        .pipe(
+          map(user => {
+            if(!user){
+              this.router.navigate(['/home']);
+                 return false
+            }
+            return true
+          })
+        )
   }
   
 }
